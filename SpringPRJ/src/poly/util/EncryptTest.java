f8e26f14c1dd381f4cec7e6a19b1450c993fe372
NahHyun-Kim <cherish3_@naver.com>
2020-12-26 01:35:19 +0900

    encryptutil commit

--- SpringPRJ/src/poly/util/EncryptTest.java
+++ SpringPRJ/src/poly/util/EncryptTest.java
@@ -17,5 +17,21 @@ public class EncryptTest {
 		System.out.println("hashEnc : " + hashEnc);
 		
 		System.out.println("------------------------");
+		System.out.println("AES128-CBC 암, 복호화 알고리즘");
+		
+		//AES128-CBC 암호화 알고리즘 실행
+		String enc = EncryptUtil.encAES128CBC(str);
+		
+		//AES128-CBC 암호화 알고리즘 결과 출력
+		System.out.println("enc : " + enc);
+		
+		//AES128-CBC 복호화 알고리즘 실행
+		String dec = EncryptUtil.decAES128CBC(enc);
+		
+		//AES128-CBC 복호화 알고리즘 결과 출력
+		System.out.println("dec : " + dec);
+		
+		System.out.println("------------------------");
+		
 	}
 }
--- SpringPRJ/src/poly/util/EncryptUtil.java
+++ SpringPRJ/src/poly/util/EncryptUtil.java
@@ -1,7 +1,20 @@
 package poly.util;
 
+import java.io.UnsupportedEncodingException;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.security.spec.AlgorithmParameterSpec;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+
+import org.apache.tomcat.util.codec.binary.Base64;
 
 public class EncryptUtil {
 
@@ -19,6 +32,13 @@ public class EncryptUtil {
 	  * AES128-CBC 암호화 알고리즘에 사용되는 초기 벡터와 암호화 키
 	  * 
 	  * */
+	 
+	 //초기 벡터(16바이트 크기를 가지며, 16바이트 단위로 암호화 시, 암호화할 총 길이가 16바이트가 되지 못하면
+	 //뒤에 추가하는 바이트)
+	 final static byte[] ivBytes = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
+	 
+	 //AES128-CBC 암호화 알고리즘에 사용되는 키(16자리 문자만 가능함)
+	 final static String key = "polyTechnic12345"; //16글자(영문자 1글자당 1바이트임)
 	 
 	 /**
 	  * 해시 알고리즘(단방향 암호화 알고리즘)-SHA-256
@@ -58,4 +78,42 @@ public class EncryptUtil {
 		 
 		 return res;
 	 }
+	 
+	 /**
+	  * AES128-CBC 암호화 함수
+	  * 
+	  * 128은 암호화 키 길이를 의미함. 128비트는 = 16바이트(1바이트 = 8비트 * 16 = 128)
+	  */
+	 public static String encAES128CBC(String str)
+	 	throws UnsupportedEncodingException, NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, 
+	 		   InvalidAlgorithmParameterException, IllegalBlockSizeException, BadPaddingException {
+		 
+		 byte[] textBytes = str.getBytes("UTF-8");
+		 AlgorithmParameterSpec ivSpec = new IvParameterSpec(ivBytes);
+		 SecretKeySpec newKey = new SecretKeySpec(key.getBytes("UTF-8"), "AES");
+		 Cipher cipher = null;
+		 cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+		 cipher.init(Cipher.ENCRYPT_MODE, newKey, ivSpec);
+		 return Base64.encodeBase64String(cipher.doFinal(textBytes));
+	 }
+	 
+	 /**
+	  * AES128-CBC 복호화 함수
+	  * 
+	  * 128은 암호화 키 길이를 의미함. 128비트는 = 16바이트(1바이트 = 8비트 * 16 = 128)
+	  */
+	 public static String decAES128CBC(String str)
+	 	throws UnsupportedEncodingException, NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, 
+	 		   InvalidAlgorithmParameterException, IllegalBlockSizeException, BadPaddingException {
+		 
+		 byte[] textBytes = Base64.decodeBase64(str);
+		 //byte[] textBytes = str.getBytes("UTF-8");
+		 AlgorithmParameterSpec ivSpec = new IvParameterSpec(ivBytes);
+		 SecretKeySpec newKey = new SecretKeySpec(key.getBytes("UTF-8"), "AES");
+		 Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+		 cipher.init(Cipher.DECRYPT_MODE, newKey, ivSpec);
+		 return new String(cipher.doFinal(textBytes), "UTF-8");
+	 }
+	 
+
 }
